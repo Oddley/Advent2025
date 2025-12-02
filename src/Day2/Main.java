ec9f1fe588cbfaa4aef608da636e20ebee30381e
Oddley <bc.spamster@gmail.com>
2025-12-02 11:26:32 -0800

    Fixed number to be output without commas

--- src/Day2/Main.java
+++ src/Day2/Main.java
@@ -8,8 +8,8 @@ void main() throws IOException {
     var lines = InputReader.ReadAllLines("Day2/input.txt");
     var ranges = InputParser.ParseLine(lines.getFirst());
 
-    PrintLine("Part 1 Answer: {0}", Part1IDValidator.Summer.SumInvalid(ranges));
-    PrintLine("Part 2 Answer: {0}", Part2IDValidator.Summer.SumInvalid(ranges));
+    PrintLine("Part 1 Answer: {0,number,#}", Part1IDValidator.Summer.SumInvalid(ranges));
+    PrintLine("Part 2 Answer: {0,number,#}", Part2IDValidator.Summer.SumInvalid(ranges));
 }
 
 void PrintLine(@NotNull String format, Object... args)
