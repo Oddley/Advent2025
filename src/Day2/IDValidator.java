b4ac4b44d71145af278c84e3e647a1d4c12706dc
Oddley <bc.spamster@gmail.com>
2025-12-02 10:22:01 -0800

    Implemented IDRange.AsList

--- src/Day2/IDValidator.java
+++ src/Day2/IDValidator.java
@@ -7,12 +7,31 @@ import java.util.List;
 public class IDValidator {
     public static boolean IsValid(int id)
     {
-        return false;
+        var string = Integer.toString(id);
+        boolean valid;
+        var half = string.length() / 2;
+        if (half * 2 != string.length())
+        {
+            valid = true;
+        } else {
+            var front = string.substring(0, half);
+            var back = string.substring(half);
+            valid = !front.equals(back);
+        }
+        return valid;
     }
 
     public static long SumInvalid(@NotNull IDRange range)
     {
-        return 0;
+        long sum = 0;
+        for (int id : range.AsList())
+        {
+            if (!IsValid(id))
+            {
+                //sum += id;
+            }
+        }
+        return sum;
     }
 
     public static long SumInvalid(@NotNull List<IDRange> ranges)
